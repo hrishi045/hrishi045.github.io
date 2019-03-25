5407514fcf6125357681855c03c27bdb5c988a24
hrishi045 <hrishi045@gmail.com>
2019-03-26 03:11:41 +0530

    Change fonts

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
-document.querySelector(".nav-toggle a").addEventListener("click", function () {
+document.querySelector(".nav-toggle").addEventListener("click", function () {
   const navs = document.querySelectorAll(".nav-link");
-  console.log(navs);
   navs.forEach(nav => nav.classList.toggle("nav-toggle-show"));
   document.querySelector(".nav-content").classList.toggle("nav-toggle-show");
 });
